d4e63bb2779404d992942d2bdb0de4f793884f3f
diegojunio <diegojunio@gmail.com>
2023-10-22 16:20:26 -0300

    Classes implementadas

--- src/main/java/com/diegojunio/todolistapp/TodolistAppApplication.java
+++ src/main/java/com/diegojunio/todolistapp/TodolistAppApplication.java
@@ -1,11 +1,14 @@
 package com.diegojunio.todolistapp;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.TimeZone;
 
+@OpenAPIDefinition(servers = {@Server(url="/", description="Default Server URL (Root)")})
 @SpringBootApplication
 public class TodolistAppApplication {
 	@PostConstruct
